7694997b2352c21db75f6a2f1facde80afb89289
defabricated <defabricated@gmail.com>
2014-10-10 19:59:34 +0200

    Implement support for ProtocolLib
    Now plugin uses ProtocolLib for sending packets.

---
+++ src/main/java/pl/defabricated/bukkittabapiplus/listeners/PacketListener.java
@@ -0,0 +1,49 @@
+package pl.defabricated.bukkittabapiplus.listeners;
+
+import com.comphenix.protocol.Packets;
+import com.comphenix.protocol.events.ConnectionSide;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.entity.Player;
+import pl.defabricated.bukkittabapiplus.TabPlugin;
+import pl.defabricated.bukkittabapiplus.api.TabList;
+import pl.defabricated.bukkittabapiplus.api.TabSlot;
+
+public class PacketListener extends PacketAdapter {
+
+    TabPlugin plugin;
+
+    public PacketListener(TabPlugin plugin) {
+        super(plugin, ConnectionSide.SERVER_SIDE, Packets.Server.PLAYER_INFO);
+        this.plugin = plugin;
+        plugin.protocolManager.addPacketListener(this);
+    }
+
+    @Override
+    public void onPacketSending(PacketEvent event){
+        if (!event.isCancelled() && event.getPacketID() == Packets.Server.PLAYER_INFO) {
+            PacketContainer packet = event.getPacket();
+            Player player = event.getPlayer();
+            int ping = packet.getIntegers().read(0);
+            if (ping == -1) {
+                TabList list = plugin.tabLists.get(player.getName());
+                ping = list.getDefaultPing();
+                String name = packet.getStrings().read(0);
+                for(int i=0; i<60; i++){
+                    TabSlot slot = list.getSlot(i);
+                    if(slot != null && slot.getName().equals(name)){
+                        ping = slot.getPing();
+                        break;
+                    }
+                }
+                packet.getIntegers().write(0, ping)
+                event.setPacket(packet);
+                return;
+            } else {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+}
--- src/main/java/pl/defabricated/bukkittabapiplus/listeners/PlayerListener.java
+++ src/main/java/pl/defabricated/bukkittabapiplus/listeners/PlayerListener.java
@@ -1,5 +1,7 @@
 package pl.defabricated.bukkittabapiplus.listeners;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
 import net.minecraft.server.v1_7_R4.PacketPlayOutPlayerInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -16,6 +18,7 @@ import pl.defabricated.bukkittabapiplus.TabPlugin;
 import pl.defabricated.bukkittabapiplus.api.TabAPI;
 import pl.defabricated.bukkittabapiplus.api.TabList;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
 
 public class PlayerListener implements Listener{
@@ -29,10 +32,16 @@ public class PlayerListener implements Listener{
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        plugin.initPlayer(event.getPlayer());
         for (Player online : Bukkit.getOnlinePlayers()) {
-            PacketPlayOutPlayerInfo packet = new PacketPlayOutPlayerInfo(online.getPlayerListName(), false, -1);
-            ((CraftPlayer) event.getPlayer()).getHandle().playerConnection.sendPacket(packet);
+            PacketContainer packet = plugin.protocolManager.createPacket(PacketType.Play.Server.PLAYER_INFO);
+            packet.getStrings().write(0, online.getPlayerListName());
+            packet.getBooleans().write(0, false);
+            packet.getIntegers().write(0, -1);
+            try {
+                plugin.protocolManager.sendServerPacket(event.getPlayer(), packet);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
         }
     }
 
